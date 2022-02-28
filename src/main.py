de0e45bd9edd6949b17a381a34036ecb158f9da2
Kym Eden <shikasta.net@gmail.com>
2022-02-28 08:38:07 +0000

    Read Wallabag unread list

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 #! /usr/bin/env python3
+
+import argparse
+import feedparser
+
+arguments = argparse.ArgumentParser(description='Wallabag to Pocket unread article sync.')
+arguments.add_argument('--host', type=str, default='app.wallabag.it', help='The Wallabag hostname')
+arguments.add_argument('--user', type=str, help='The Wallabag user')
+arguments.add_argument('--secret', type=str, help='The Wallabag user\'s secret')
+
+config = arguments.parse_args()
+
+wallabag_feed = feedparser.parse(F"https://{config.host}/feed/{config.user}/{config.secret}/unread")
