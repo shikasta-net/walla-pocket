76f7429eb4fc04ddd1d465b6845addcd987a76f0
Kym Eden <shikasta.net@gmail.com>
2022-02-28 13:55:24 +0000

    Compare to Pocket and archive/add entries

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Wallabag to Pocket sync.
 Kobo has Pocket integration and is great for reading your collected articles, unless you use any other save-for-later service.
 
 Simple clone sync that mirrors all changes in your unread list to Pocket, removing read items.
+
+To generate a Pocket access token for your API connection, you'll probably need something like https://reader.fxneumann.de/plugins/oneclickpocket/auth.php
--- src/main.py
+++ src/main.py
@@ -2,12 +2,61 @@
 
 import argparse
 import feedparser
+import json
+import pocket
 
 arguments = argparse.ArgumentParser(description='Wallabag to Pocket unread article sync.')
 arguments.add_argument('--host', type=str, default='app.wallabag.it', help='The Wallabag hostname')
 arguments.add_argument('--user', type=str, help='The Wallabag user')
 arguments.add_argument('--secret', type=str, help='The Wallabag user\'s secret')
+arguments.add_argument('--pocket-key', type=str, help='The Pocket API key')
+arguments.add_argument('--pocket-secret', type=str, help='The Pocket API secret/token')
 
 config = arguments.parse_args()
 
-wallabag_feed = feedparser.parse(F"https://{config.host}/feed/{config.user}/{config.secret}/unread")
+pocket_connection = pocket.Pocket(
+  consumer_key=config.pocket_key,
+  access_token=config.pocket_secret
+)
+
+def __wallabag_unread_articles():
+  wallabag_feed = feedparser.parse(F"https://{config.host}/feed/{config.user}/{config.secret}/unread")
+  return { entry.links[0]['href']: entry for entry in reversed(wallabag_feed.entries) }
+
+def __pocket_unread_articles():
+  try:
+    pocket_feed = pocket_connection.retrieve(state='unread')['list']
+  except pocket.PocketException as e:
+    print(e.message)
+  unread_articles = pocket_feed.values() if pocket_feed else []
+  return { entry['given_url'] : entry for entry in unread_articles }
+
+def __remove_articles_from_pocket(pocket_articles):
+  for url, entry in pocket_articles.items():
+    pocket_connection.delete(entry['item_id'])
+  try:
+    pocket_connection.commit()
+  except pocket.PocketException as e:
+    print(e.message)
+
+def __archive_missing_articles(pocket_entries, urls_of_unread_articles):
+  for url, entry in pocket_entries.items():
+    if url not in urls_of_unread_articles:
+      pocket_connection.archive(entry['item_id'])
+  try:
+    pocket_connection.commit()
+  except pocket.PocketException as e:
+    print(e.message)
+
+def __add_new_articles(unread_articles, urls_of_unread_articles):
+  for url, article in unread_articles.items():
+    if url not in urls_of_unread_articles:
+      pocket_connection.add(url)
+
+if __name__ == "__main__":
+  unread_articles = __wallabag_unread_articles()
+  pocket_entries = __pocket_unread_articles()
+
+  __archive_missing_articles(pocket_entries, unread_articles.keys())
+
+  __add_new_articles(unread_articles, pocket_entries.keys())
